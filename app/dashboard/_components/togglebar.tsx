5eeaa5c6c8719f09243ed8bda0a2876ab3e67df4
Zakir Matloob <145646219+ZakirCodeArchitect@users.noreply.github.com>
2024-09-02 02:40:43 +0500

    Update togglebar.tsx

--- app/dashboard/_components/togglebar.tsx
+++ app/dashboard/_components/togglebar.tsx
@@ -1,25 +1,25 @@
-import React, { useState } from 'react';
-import Header from './Header';
-import SideNav from './sideNav';
+// import React, { useState } from 'react';
+// import Header from './Header';
+// import SideNav from './sideNav';
 
-function AppLayout() {
-  const [sidebarOpen, setSidebarOpen] = useState(false);
+// function AppLayout() {
+//   const [sidebarOpen, setSidebarOpen] = useState(false);
 
-  const toggleSidebar = () => {
-    setSidebarOpen(!sidebarOpen);
-  };
+//   const toggleSidebar = () => {
+//     setSidebarOpen(!sidebarOpen);
+//   };
 
-  return (
-    <div className="relative min-h-screen flex">
-      <SideNav isOpen={sidebarOpen} toggleSidebar={toggleSidebar} />
-      <div className="flex-grow">
-        <Header toggleSidebar={toggleSidebar} />
-        <main className="p-5">
-          {/* Main content goes here */}
-        </main>
-      </div>
-    </div>
-  );
-}
+//   return (
+//     <div className="relative min-h-screen flex">
+//       <SideNav isOpen={sidebarOpen} toggleSidebar={toggleSidebar} />
+//       <div className="flex-grow">
+//         <Header toggleSidebar={toggleSidebar} />
+//         <main className="p-5">
+//           {/* Main content goes here */}
+//         </main>
+//       </div>
+//     </div>
+//   );
+// }
 
-export default AppLayout;
+// export default AppLayout;
