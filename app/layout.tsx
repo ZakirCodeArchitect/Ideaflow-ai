e26977ff0ebff20f5fac9a19336c40e7cd3c7172
Zakir Matloob <145646219+ZakirCodeArchitect@users.noreply.github.com>
2024-09-02 14:44:25 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter, Outfit } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Outfit({ subsets: ["latin"] });
 
